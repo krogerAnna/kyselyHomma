87622af656ef44b82fd146b7a8635a9acfcb9f6c
a1704532 <mikael.kotila@wippies.fi>
2019-10-29 14:38:34 +0200

    moro

--- kyselyHomma/src/main/java/hh/swd/kyselyHomma/KyselyHommaApplication.java
+++ kyselyHomma/src/main/java/hh/swd/kyselyHomma/KyselyHommaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KyselyHommaApplication {
 
-	////////
+	////////:))))
 	public static void main(String[] args) {
 		SpringApplication.run(KyselyHommaApplication.class, args);
 	}
