b93d7b19181e886715f43863b2aa6afed076c50e
a1704532 <mikael.kotila@wippies.fi>
2019-10-29 19:12:14 +0200

    Kysymysten lisääminen

--- kyselyHomma/src/main/java/hh/swd/kyselyHomma/web/KysymysController.java
+++ kyselyHomma/src/main/java/hh/swd/kyselyHomma/web/KysymysController.java
@@ -2,13 +2,18 @@ package hh.swd.kyselyHomma.web;
 
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import hh.swd.kyselyHomma.domain.Kysymys;
 import hh.swd.kyselyHomma.domain.KysymysRepository;
 
-@RestController
+//@RestController
+@Controller
 public class KysymysController {
 
 	private final KysymysRepository repository;
@@ -21,5 +26,15 @@ public class KysymysController {
 	List<Kysymys> all() {
 		return repository.findAll();
 	}
-	
+	@GetMapping("/lisaakysymys")
+	public String lisaaKysymys(Model model) {
+		model.addAttribute("kysymys", new Kysymys());
+		model.addAttribute("kysymykset", all());
+		return "lisaakysymys";
+	}
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Kysymys kysymys) {
+		repository.save(kysymys);
+		return "redirect:lisaakysymys";
+	}
 }
