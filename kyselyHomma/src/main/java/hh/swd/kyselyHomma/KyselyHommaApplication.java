1a6e08cb0865e7fb729369dc27d351d16b6a6a7e
Halfeen <anna1.kroger@gmail.com>
2019-10-29 13:24:42 +0200

    projektin aloitus

---
+++ kyselyHomma/src/main/java/hh/swd/kyselyHomma/KyselyHommaApplication.java
@@ -0,0 +1,13 @@
+package hh.swd.kyselyHomma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyselyHommaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyselyHommaApplication.class, args);
+	}
+
+}
