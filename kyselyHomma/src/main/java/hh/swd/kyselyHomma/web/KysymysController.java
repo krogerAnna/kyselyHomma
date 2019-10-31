243c8b2bf98b7f81d5e5077ac76097306a83c41a
a1704532 <mikael.kotila@wippies.fi>
2019-10-31 13:31:24 +0200

    Metodi welcome-sivulle

--- kyselyHomma/src/main/java/hh/swd/kyselyHomma/web/KysymysController.java
+++ kyselyHomma/src/main/java/hh/swd/kyselyHomma/web/KysymysController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -22,10 +21,19 @@ public class KysymysController {
 		this.repository = repository;
 	}
 	
+	//Palauttaa etusivu
+	@GetMapping("/")
+	public String index() {
+		return "welcome";
+	}
+	
+	//Etsii ja palauttaa kaikki kysymykset
 	@GetMapping("/kysymykset")
 	@ResponseBody List<Kysymys> kysymykset() {
 		return repository.findAll();
 	}
+	
+	//Annetaan model attributet Thymeleaf-templatelle
 	@GetMapping("/lisaakysymys")
 	public String lisaaKysymys(Model model) {
 		model.addAttribute("kysymys", new Kysymys());
@@ -33,16 +41,10 @@ public class KysymysController {
 		return "lisaakysymys";
 	}
 	
+	//Tallentaa uuden kysymyksen
 	@PostMapping("/save")
 	public String save(Kysymys kysymys) {
 		repository.save(kysymys);
 		return "redirect:lisaakysymys";
-	}
-	
-	//delete
-	@GetMapping("/poistakysymys/{id}")
-	public String poistaKysymys(@PathVariable("id") Long kysymysId, Model model) {
-		repository.deleteById(kysymysId);
-		return "redirect:../lisaakysymys";
 	}
 }
