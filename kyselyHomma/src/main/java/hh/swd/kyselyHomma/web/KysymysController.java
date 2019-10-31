42ce5284869f4195342c733abe2a0985aa6a756f
a1704532 <mikael.kotila@wippies.fi>
2019-10-31 12:36:51 +0200

    Molemmat toimii

--- kyselyHomma/src/main/java/hh/swd/kyselyHomma/web/KysymysController.java
+++ kyselyHomma/src/main/java/hh/swd/kyselyHomma/web/KysymysController.java
@@ -5,34 +5,34 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import hh.swd.kyselyHomma.domain.Kysymys;
 import hh.swd.kyselyHomma.domain.KysymysRepository;
-
-//@RestController
+ 
 @Controller
 public class KysymysController {
 
 	private final KysymysRepository repository;
 	
-	KysymysController(KysymysRepository repository) {
+	
+	KysymysController (KysymysRepository repository) {
 		this.repository = repository;
 	}
 	
 	@GetMapping("/kysymykset")
-	List<Kysymys> all() {
+	@ResponseBody List<Kysymys> kysymykset() {
 		return repository.findAll();
 	}
 	@GetMapping("/lisaakysymys")
 	public String lisaaKysymys(Model model) {
 		model.addAttribute("kysymys", new Kysymys());
-		model.addAttribute("kysymykset", all());
+		model.addAttribute("kysymykset", kysymykset());
 		return "lisaakysymys";
 	}
-	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	
+	@PostMapping("/save")
 	public String save(Kysymys kysymys) {
 		repository.save(kysymys);
 		return "redirect:lisaakysymys";
