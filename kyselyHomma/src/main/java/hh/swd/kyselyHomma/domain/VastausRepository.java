07cf487b8407682c231655af076530e389418325
Leevih <leevi.honkonen@gmail.com>
2019-11-07 13:17:10 +0200

    first commit

---
+++ kyselyHomma/src/main/java/hh/swd/kyselyHomma/domain/VastausRepository.java
@@ -0,0 +1,11 @@
+package hh.swd.kyselyHomma.domain;
+
+//import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.repository.CrudRepository;
+
+public interface VastausRepository extends JpaRepository<Vastaus, Long> {
+
+	//List<Kysymys> findByContent(String content);
+}
--- kyselyHomma/src/main/java/hh/swd/kyselyHomma/web/KysymysController.java
+++ kyselyHomma/src/main/java/hh/swd/kyselyHomma/web/KysymysController.java
@@ -2,17 +2,18 @@ package hh.swd.kyselyHomma.web;
 
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import hh.swd.kyselyHomma.domain.Kysymys;
 import hh.swd.kyselyHomma.domain.KysymysRepository;
  
-@Controller
+@RestController
 public class KysymysController {
 
 	private final KysymysRepository repository;
@@ -42,6 +43,11 @@ public class KysymysController {
 		return "lisaakysymys";
 	}
 	
+	@PostMapping("/kysymykset")
+	Kysymys newKysymys(@RequestBody Kysymys newKysymys) {
+	    return repository.save(newKysymys);
+	  }
+	
 	//Tallentaa uuden kysymyksen
 	@PostMapping("/save")
 	public String save(Kysymys kysymys) {
@@ -52,7 +58,7 @@ public class KysymysController {
 	//Poista kysymys
 	@GetMapping("/poistakysymys/{id}")
 	public String delete(@PathVariable("id") Long id) {
-		repository.deleteById(id); //testikommentti
+		repository.deleteById(id);
 		return "redirect:../lisaakysymys";
 	}
 	
