56a5dde97c76781b845d786c55f18d6308b84634
Halfeen <anna1.kroger@gmail.com>
2019-11-12 13:26:56 +0200

    vastausjutut application hommassa

--- kyselyHomma/src/main/java/hh/swd/kyselyHomma/KyselyHommaApplication.java
+++ kyselyHomma/src/main/java/hh/swd/kyselyHomma/KyselyHommaApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 
 import hh.swd.kyselyHomma.domain.Kysymys;
 import hh.swd.kyselyHomma.domain.KysymysRepository;
+import hh.swd.kyselyHomma.domain.Vastaus;
+import hh.swd.kyselyHomma.domain.VastausRepository;
 
 @SpringBootApplication
 public class KyselyHommaApplication {
@@ -15,15 +17,31 @@ public class KyselyHommaApplication {
 		SpringApplication.run(KyselyHommaApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner demo(KysymysRepository repository) {
+	public CommandLineRunner demo(KysymysRepository kysymysRepo, VastausRepository vastausRepo) {
 		return (args)  -> {	
-			repository.save(new Kysymys("Status?"));
-			repository.save(new Kysymys("Kuinka usein asioit kouluruokalassa?"));
-			repository.save(new Kysymys("Jos et asioi ollenkaan tai todella harvoin niin miksi?"));
-			repository.save(new Kysymys("Mikä lisäisi/parantaisi ruokalassa asioimistasi?"));
-			repository.save(new Kysymys("Jos vastasit äskeiseen kysymykseen 'Muu' kirjoita alle mitä se olisi:"));
-			repository.save(new Kysymys("Halutessasi voit kirjoittaa tähän lisätietoja koskien vastauksiasi tai, jos mielessäsi on jotain, mitä emme tulleet kysyneeksi:"));
-
+			
+			Kysymys q = new Kysymys("Status?");
+			Kysymys w = new Kysymys("Kuinka usein asioit kouluruokalassa?");
+			Kysymys e = new Kysymys("Jos et asioi ollenkaan tai todella harvoin niin miksi?");
+			Kysymys r = new Kysymys("Mikä lisäisi/parantaisi ruokalassa asioimistasi?");
+			Kysymys t = new Kysymys("Jos vastasit äskeiseen kysymykseen 'Muu' kirjoita alle mitä se olisi:");
+			Kysymys y = new Kysymys("Halutessasi voit kirjoittaa tähän lisätietoja koskien vastauksiasi tai, jos mielessäsi on jotain, mitä emme tulleet kysyneeksi:");
+			
+			kysymysRepo.save(q);
+			kysymysRepo.save(w);
+			kysymysRepo.save(e);
+			kysymysRepo.save(r);
+			kysymysRepo.save(t);
+			kysymysRepo.save(y);
+			
+			vastausRepo.save(new Vastaus("Opiskelija", q));
+			vastausRepo.save(new Vastaus("En ikinä", w));
+			vastausRepo.save(new Vastaus("Olen nirso", e));
+			vastausRepo.save(new Vastaus("Parempi ruoka", r));
+			vastausRepo.save(new Vastaus("-", t));
+			vastausRepo.save(new Vastaus("Ei tule mitään mieleen.", y));
+			
+			
 		
 		};
 	}
