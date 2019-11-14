5a47a09ae55864f7f51ac3ef4903ed08efb538bd
Leevih <leevi.honkonen@gmail.com>
2019-11-14 11:31:54 +0200

    .

--- kyselyHomma/src/main/java/hh/swd/kyselyHomma/web/VastausController.java
+++ kyselyHomma/src/main/java/hh/swd/kyselyHomma/web/VastausController.java
@@ -1,10 +1,13 @@
 package hh.swd.kyselyHomma.web;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import hh.swd.kyselyHomma.domain.Vastaus;
 import hh.swd.kyselyHomma.domain.VastausRepository;
 
+
+@Controller
 public class VastausController {
 
 	private final VastausRepository repository;
