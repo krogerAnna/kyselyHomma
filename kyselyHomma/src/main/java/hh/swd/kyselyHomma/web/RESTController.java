1156595d74e5c29aecba8bc4b4353c5c918e0add
Halfeen <anna1.kroger@gmail.com>
2019-11-19 13:03:55 +0200

    varmistetaan että kaikki on ajantasalla

--- kyselyHomma/src/main/java/hh/swd/kyselyHomma/web/RESTController.java
+++ kyselyHomma/src/main/java/hh/swd/kyselyHomma/web/RESTController.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import hh.swd.kyselyHomma.domain.Kysely;
 import hh.swd.kyselyHomma.domain.KyselyRepository;
 import hh.swd.kyselyHomma.domain.Kysymys;
