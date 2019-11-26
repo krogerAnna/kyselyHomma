10a6c892a03af911539687502aeaaf20317097a4
Roope Laakso <roope.laakso@gmail.com>
2019-11-26 15:44:56 +0200

    kyselyn vastaukset voi hakea

--- kyselyHomma/src/main/java/hh/swd/kyselyHomma/web/KyselyController.java
+++ kyselyHomma/src/main/java/hh/swd/kyselyHomma/web/KyselyController.java
@@ -1,5 +1,6 @@
 package hh.swd.kyselyHomma.web;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,44 +14,74 @@ import hh.swd.kyselyHomma.domain.Kysely;
 import hh.swd.kyselyHomma.domain.KyselyRepository;
 import hh.swd.kyselyHomma.domain.Kysymys;
 import hh.swd.kyselyHomma.domain.KysymysRepository;
+import hh.swd.kyselyHomma.domain.Vastaus;
+import hh.swd.kyselyHomma.domain.VastausRepository;
 
 @Controller
 public class KyselyController {
 
-	
 	// **** Kysely RESTit **** //
-	  
-	  @Autowired
-		private KyselyRepository kyselyRepo;
-	  @Autowired
-	  private KysymysRepository kysymysRepo;
+
+	@Autowired
+	private KyselyRepository kyselyRepo;
+	@Autowired
+	private KysymysRepository kysymysRepo;
+//	@Autowired
+//	private VastausRepository vastausRepo;
+
+	// Etsii ja palauttaa kaikki kyselyt
+	@GetMapping("/kyselyt")
+	@ResponseBody
+	List<Kysely> kyselyt() {
+		return kyselyRepo.findAll();
+	}
+
+	// getById
+	@GetMapping("/kyselyt/{id}")
+	public @ResponseBody Optional<Kysely> findKysely(@PathVariable("id") Long kyselyId) {
+		return kyselyRepo.findById(kyselyId);
+	}
+
+	// kaikki x-kyselyn kysymykset
+
+	// Etsii ja palauttaa kaikki kysymykset
+	// @GetMapping("/kysymykset")
+	// @ResponseBody List<Kysymys> kysymykset() {
+	// return kysymysRepo.findAll();
+	// }
+
+	@GetMapping("/kyselyt/{id}/kysymykset")
+	public @ResponseBody List<Kysymys> findByKysely(@PathVariable("id") Long kyselyId) {
+		Optional<Kysely> kysely = kyselyRepo.findById(kyselyId);
+		return kysymysRepo.findAllByKysely(kysely);
+	}// ei toimi vielä
+
+	
+	@GetMapping("kyselyt/{id}/vastaukset")
+	public @ResponseBody List<Vastaus> findVastauksetByKysely(@PathVariable("id") Long kyselyId) {
 		
-		// Etsii ja palauttaa kaikki kyselyt
-		@GetMapping("/kyselyt")
-		@ResponseBody List<Kysely> kyselyt() {
-			return kyselyRepo.findAll();
+		// haetaan kysymykset kyselyn perusteella
+		
+		Optional<Kysely> kysely = kyselyRepo.findById(kyselyId);
+		List<Kysymys> kysymykset = kysymysRepo.findAllByKysely(kysely);
+		List<Vastaus> vastaukset = new ArrayList<Vastaus>();
+		List<Vastaus> kysymyksenVastaukset = new ArrayList<Vastaus>();
+		
+		// käydään taulukko läpi 
+		// jokaisen kysymys-olion kohdalla haetaan sen sisältämät vastaukset ja talletetaan ne muuttujaan
+		// sitten iteroidaan muuttujan "kysymyksenVastaukset", joka on taulukko, läpi ja lisätään jokainen sen sisältämä
+		// vastaus muuttujaan "vastaukset"
+		// lopulta tämä palautetaan
+		
+		for (Kysymys kysymys : kysymykset) {
+			kysymyksenVastaukset = kysymys.getVastaukset();
+			for (Vastaus vastaus : kysymyksenVastaukset) {
+				vastaukset.add(vastaus);
+			}
+			
 		}
 		
-		// getById
-		@GetMapping("/kyselyt/{id}")
-		public @ResponseBody Optional<Kysely> findKysely(@PathVariable("id") Long kyselyId) {
-			return kyselyRepo.findById(kyselyId);
-		}
-		
-		
-		// kaikki x-kyselyn kysymykset
-		
-			// Etsii ja palauttaa kaikki kysymykset
-							//@GetMapping("/kysymykset")
-							//@ResponseBody List<Kysymys> kysymykset() {
-							//	return kysymysRepo.findAll();
-							//}
-							
-		@GetMapping("/kyselyt/{id}/kysymykset")
-		public @ResponseBody List<Kysymys> findByKysely(@PathVariable("id") Long kyselyId) {
-			Optional<Kysely> kysely = kyselyRepo.findById(kyselyId);
-			return kysymysRepo.findAllByKysely(kysely);
-		}//ei toimi vielä
-	
-	
+		return vastaukset;
+	}
+
 }
